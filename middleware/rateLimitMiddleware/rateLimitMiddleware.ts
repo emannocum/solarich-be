2773b838260a18e61df942c79391a4de564731a2
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-08-03 20:43:17 +0800

    added waitime for seconds, mins and hrs

--- middleware/rateLimitMiddleware/rateLimitMiddleware.ts
+++ middleware/rateLimitMiddleware/rateLimitMiddleware.ts
@@ -1,23 +1,48 @@
-import {Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
+
+interface UserLimitData {
+  count: number;
+  firstRequestTime: number;
+}
 
 // Store the user usage limit (you can use a database for more persistent storage)
-const userLimits = new Map();
+const userLimits = new Map<string, UserLimitData>();
 
-export const rateLimitMiddleware = (req : Request, res : Response, next : NextFunction) => {
+export const rateLimitMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const { receiver } = req.body;
-  console.log(receiver)
+  console.log(receiver);
   if (!receiver) {
     return res.status(400).json({ error: 'Email is not provided.' });
   }
 
-  const limit = userLimits.get(receiver) || 0;
+  const currentTime = Date.now();
+  const limitData = userLimits.get(receiver);
 
-  if (limit >= 2) {
-    return res.status(429).json({ error: 'Too many requests. Please try again later.' });
+  if (!limitData) {
+    // First request from the user
+    userLimits.set(receiver, { count: 1, firstRequestTime: currentTime });
+    return next();
   }
 
-  userLimits.set(receiver, limit + 1);
+  const { count, firstRequestTime } = limitData;
+  const limit = 2; // The maximum number of requests allowed
+
+  if (count >= limit) {
+    const waitTimeInSeconds = Math.ceil((firstRequestTime + 3600000 - currentTime) / 1000);
+
+    // Calculate wait time in hours and minutes
+    const waitTimeInHours = Math.floor(waitTimeInSeconds / 3600);
+    const remainingSeconds = waitTimeInSeconds % 3600;
+    const waitTimeInMinutes = Math.floor(remainingSeconds / 60);
+
+    return res.status(429).json({
+      error: 'Too many requests. Please try again later.',
+      waitTimeInSeconds,
+      waitTimeInHours,
+      waitTimeInMinutes
+    });
+  }
+
+  userLimits.set(receiver, { count: count + 1, firstRequestTime });
   next();
 };
-
-
